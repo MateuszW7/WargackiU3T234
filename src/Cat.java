b28681b211a9739b87d12ff419ce4d121ad83016
BT_3W2_34 <mateuszw7@nycstudents.net>
2022-10-28 12:20:04 -0400

    Initial Commit 2

--- src/Cat.java
+++ src/Cat.java
@@ -1,2 +1,36 @@
-package PACKAGE_NAME;public class Cat {
+
+public class Cat
+{
+    private String name;
+    private int age;
+
+    public Cat(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+
+    public String speak() {
+        int random = (int) (Math.random() * 4) +1;
+        if (random == 1){
+            return "meow!";
+        } else if (random ==2) {
+            return "purrr!";
+        } else {
+            return "growl!";
+        }
+    }
+
+    public void introduce() {
+        System.out.println("Hello. My name is " + name + " and I am " + age + " years old. Now, mind your own business.");
+    }
 }
--- src/CatHouse.java
+++ src/CatHouse.java
@@ -1,2 +1,46 @@
-package PACKAGE_NAME;public class CatHouse {
+
+public class CatHouse {
+    private Cat cat1;
+    private Cat cat2;
+    private String owner;
+
+    public CatHouse(Cat cat1, Cat cat2, String owner) {
+        this.cat1 = cat1;
+        this.cat2 = cat2;
+        this.owner = owner;
+    }
+
+    public void rollCall() {
+        if (cat1.getAge() == cat2.getAge()){
+            String cat1Name = cat1.getName();
+            String cat2Name = cat2.getName();
+            if (cat1Name.compareTo(cat2Name) <= 0){
+                cat1.introduce();
+                cat2.introduce();
+            } else {
+                cat2.introduce();
+                cat1.introduce();
+            }
+        } else if (cat1.getAge() > cat2.getAge()) {
+            cat1.introduce();
+            cat2.introduce();
+        } else {
+            cat2.introduce();
+            cat1.introduce();
+        }
+    }
+
+    public void catNoise() {
+        System.out.println(cat1.speak());
+        System.out.println(cat2.speak());
+    }
+
+    // MODIFY this so that it returns a string with each Cat's AGE next to its name
+    // e.g. Cat 1's Name: Petunia, Age: 3
+    public String houseInfo() {
+        String str = "Welcome to " + owner + "'s Cat House!\n";
+        str += "Cat 1's Name: " + cat1.getName() + ", Age: " + cat1.getAge() + "\n";
+        str += "Cat 2's Name: " + cat2.getName() + ", Age: " + cat2.getAge();
+        return str;
+    }
 }
